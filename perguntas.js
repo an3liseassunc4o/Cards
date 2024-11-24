d6c41b572d7fee7ca208af9caf9b5c0eb21744ad
Anelise Assunção <aneliseaps@gmail.com>
2024-11-24 10:29:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matematica',
+    '2+2?',
+    '4'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o nome da energia tirada dos ventos?',
+    'Eolica'
+)
+
+criaCartao(
+    'Artes',
+    'O Azul + Vermelho?',
+    'Roxo'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Pato em Inglês?',
+    'Duck'
+)
